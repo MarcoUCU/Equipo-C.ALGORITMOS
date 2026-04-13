b835d42ff0e097e123396a519043ff2dd1d6a2e9
santiago-ripa <santiagoripa@correo.ucu.edu.uy>
2026-04-13 10:36:38 -0300

    arreglo ej26

--- Ejercicio 26/src/main/java/com/example/Expresion.java
+++ Ejercicio 26/src/main/java/com/example/Expresion.java
@@ -2,8 +2,6 @@ package com.example;
 
 import java.util.List;
 import java.util.Stack;
-//No uso las clase sque hice de pilas y eso porque en la letra decia "Este método deberá ser
-//desarrollado utilizando la clase Stack de Java."
 public class Expresion {
     public boolean controlCorchetes(List<Character> listaDeEntrada){
         Stack<Character> pila = new Stack<>();
